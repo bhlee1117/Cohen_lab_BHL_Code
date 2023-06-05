68949ab1233530445785953d04f88fbea6ff6e35
Byung Hun Lee <bhlee1117@gmail.com>
2023-06-04 22:59:19 -0400

    BHL_060423

---
+++ Cell_segment_circle_10x_VU.m
@@ -0,0 +1,29 @@
+function [centers, radii]=Cell_segment_circle_10x_VU(avgImg,thres)
+%% Control Tower
+if nargin<4
+
+end
+
+%%
+
+avgImg=mat2gray(avgImg-medfilt2(avgImg,[150 150],"symmetric"));
+im_G=imgaussfilt(avgImg,1.5);
+
+%%
+detectCircles = @(x) imfindcircles(x,[4 5], ...
+    'Sensitivity',thres, ...
+    'EdgeThreshold',0.03, ...
+    'Method','TwoStage', ...
+    'ObjectPolarity','Bright');
+[centers, radii, metric] = detectCircles(im_G);
+% imagesc(255-imfilt)
+% colormap('gray')
+% hold all
+% for i=1:size(centers,1)
+%     plot(centers(i,1),centers(i,2),'ro')
+%     text(centers(i,1),centers(i,2)+10,num2str(i),'color','r',...
+% 'fontweight','bold')
+% end
+%
+% axis equal
+end
