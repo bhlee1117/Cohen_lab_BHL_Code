a0b28155fe64f60ac2612ec0746e983233555632
Byung Hun Lee <bhlee1117@gmail.com>
2026-07-15 17:46:59 -0400

    20260715

--- show_footprnt.m
+++ show_footprnt.m
@@ -1,23 +1,25 @@
-function mergedImg=show_footprnt(c_ftprnt,mov_mc,colr)
-coord=get_coord(c_ftprnt>0);
-if nargin<3
-    colr = max(colormap(turbo(size(c_ftprnt,3))),0); colr(colr>1)=1;
-end
-%figure; tiledlayout(2,1)
-%a=randn(size(c_ftprnt,3),1); [~,b]=sort(a);
-c_ftprnt=toimg(rescale2(tovec(c_ftprnt),1),size(c_ftprnt,1),size(c_ftprnt,2));
-c_ftprnt=mat2gray(c_ftprnt);
-mov_mc=mat2gray(mov_mc);
-b=[1:size(c_ftprnt,3)];
-ax1=nexttile([1 1]);
-%ax1=subplot(2,1,1);
+function mergedImg = show_footprnt(c_ftprnt, mov_mc, colr)
+    [H, W, N] = size(c_ftprnt);
 
-mergedImg=squeeze(sum(c_ftprnt.*reshape(colr(b,:),1,1,[],3),3));
-imshow2(mergedImg,[]);
-%text(coord(:,1)',coord(:,2)',num2str([1:size(c_ftprnt,3)]'),'color','w')
+    if nargin < 3 || isempty(colr)
+        colr = max(turbo(N), 0);
+        colr(colr>1) = 1;
+    end
 
-%ax2=subplot(2,1,2);
-ax2=nexttile([1 1]);
-imshow2(imfuse(mean(mov_mc,3),sum(c_ftprnt,3)),[])
-linkaxes([ax1 ax2],'xy')
+    % Normalize footprints
+    c_ftprnt = toimg(rescale2(tovec(c_ftprnt),1), H, W);
+    c_ftprnt = mat2gray(c_ftprnt);
+    mov_mc   = mat2gray(mov_mc);
+
+    % ---- Fast colored composite: matrix multiply instead of broadcast+sum ----
+    % (H*W x N) * (N x 3) -> (H*W x 3) -> H x W x 3
+    Cvec      = reshape(c_ftprnt, H*W, N);
+    mergedImg = reshape(Cvec * colr(1:N,:), H, W, 3);
+
+    ax1 = nexttile([1 1]);
+    imshow2(mergedImg, []);
+
+    ax2 = nexttile([1 1]);
+    imshow2(imfuse(mean(mov_mc,3), sum(c_ftprnt,3)), []);
+    linkaxes([ax1 ax2],'xy')
 end
